19aeba4c074cccdde6d67981e007a0525be027ee
Sherkhan Tulkibay <sherkhan-92@bk.ru>
2021-12-09 21:28:42 +0300

    fix database

--- config/server.js
+++ config/server.js
@@ -1,6 +1,6 @@
 module.exports = ({ env }) => ({
-  host: process.env.HOST,
-  port: process.env.PORT,
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
   admin: {
     auth: {
       secret: env('ADMIN_JWT_SECRET', '5b931f85088a0780c9af45f01ecad22c'),
